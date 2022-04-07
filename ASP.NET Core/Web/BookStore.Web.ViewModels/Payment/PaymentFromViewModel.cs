779a57353ba96c25c1dcfcb1759e78c02d94ee8a
Anzhela Nurieva <anzzhhela98@gmail.com>
2022-04-08 00:00:52 +0300

    Payment

---
+++ ASP.NET Core/Web/BookStore.Web.ViewModels/Payment/PaymentFromViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Web.ViewModels.Payment
+{
+    public class PaymentFromViewModel
+    {
+        public int Count { get; set; }
+
+        public string FullName { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+
+        public string Number { get; set; }
+
+        public long TotalPriceTransfer { get; set; }
+
+        public string StripeToken { get; set; }
+
+        public string StripeEmail { get; set; }
+    }
+}
--- ASP.NET Core/Web/BookStore.Web/Controllers/Payment/PaymentController.cs
+++ ASP.NET Core/Web/BookStore.Web/Controllers/Payment/PaymentController.cs
@@ -1,8 +1,10 @@
 ﻿namespace BookStore.Web.Controllers.Payment
 {
+    using BookStore.Web.ViewModels.Payment;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Stripe;
+    using System.Collections.Generic;
 
     public class PaymentController : Controller
     {
@@ -11,42 +13,59 @@
             return this.View();
         }
 
-        //[HttpGet]
-        //[Authorize]
-        //public IActionResult Charge()
-        //{
-        //    return this.View();
-        //}
-
         [HttpPost]
         [Authorize]
-        public IActionResult Charge(string stripeEmail, string stripeToken, string id, string BookId)
+        public IActionResult Charge(PaymentFromViewModel model)
         {
-            ;
             var customers = new CustomerService();
             var charges = new ChargeService();
 
             var customer = customers.Create(new CustomerCreateOptions
             {
-                Email = stripeEmail,
+                Email = model.StripeEmail,
             });
-
+            ;
             var charge = charges.Create(new ChargeCreateOptions
             {
-                Amount = 600 * 100,
+                Amount = model.TotalPriceTransfer / 100,
                 Description = "Test Payment",
                 Currency = "EUR",
-                Source = stripeToken,
+                Source = model.StripeToken,
+                ReceiptEmail = model.StripeEmail,
             });
-            
+            ;
 
             if (charge.Status == "succeeded")
             {
                 string balanceTransactionId = charge.BalanceTransactionId;
+                //StripeConfiguration.ApiKey = "sk_test_51KkssRBK8vsMODVi3jyPUx64V0x8FkSa4AhKavRxq8I69ExETswbwE8SJnMG1XqkvbTvsUnQh592k7Vt4iS8PA5e0001UEjQGV";
+
+                //var paymentIntents = new PaymentIntentService();
+                //var createOptions = new PaymentIntentCreateOptions
+                //{
+                //    Amount = model.TotalPriceTransfer / 100,
+                //    Currency = "eur",
+                //    PaymentMethodTypes = new List<string> { "card" },
+                //    ReceiptEmail = model.StripeEmail,
+                //};
+
+                //paymentIntents.Create(createOptions);
+
+                ////var myCharge = new StripeChargeCreateOptions
+                ////{
+                ////    CustomerId = stripeCustomer.Id,
+                ////    Currency = "gbp",
+                ////    Amount = 1000,
+                ////    ReceiptEmail = "customer@email.com"
+                ////};
+                ////var chargeService = new StripeC();
+                //var stripeCharge = charges.Create(charge);
+
+                ;
             }
             else
             {
-                return this.View(); //TO DO..
+                return this.Redirect("~/Payment/ErrorMessage"); //TO DO..
             }
 
             return this.Redirect("~/Home/Index");
